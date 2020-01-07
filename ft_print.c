4db10ed5ba4cdab24cdaedb65b9fd21a37314020
marie-nde <marie9nde@gmail.com>
2020-01-07 15:44:00 +0100

    print hexa removed

--- ft_print.c
+++ ft_print.c
@@ -26,31 +26,3 @@ void	ft_print_nbr(int nb)
 		ft_print_nbr(n / 10);
 	ft_print_char((n % 10) + '0');
 }
-
-void	ft_print_hexa(int nb, char c)
-{
-	long n;
-	char *low;
-	char *caps;
-
-	n = nb;
-	low = "0123456789abcdef";
-	caps = "0123456789ABCDEF";
-	if (n < 0)
-	{
-		ft_print_char('-');
-		n = -n;
-	}
-	if (c == 'x')
-	{
-		if (n > 9)
-			ft_print_hexa(n / 16, c);
-		ft_print_char(low[n % 16] + '0');
-	}
-	else if (c == 'X')
-	{
-		if (n > 9)
-			ft_print_hexa(n / 16, c);
-		ft_print_char(caps[n % 16] + '0');
-	}
-}
--- main.c
+++ main.c
@@ -11,5 +11,29 @@ int		main()
 	printf("|%d|", 42);
 	printf("\n\nMy printf\n");
 /*	ft_printf("|%d|\n", 42);*/
+	ft_print_hexa(-42, 'x');
+
+	printf("\n\n|%d|\n", 2147483647);
+	printf("|%d|\n", -2147483648);
+	printf("|%d|\n", 4294967295);
+	printf("|%u|\n", 4294967295);
+	printf("|%u|\n", 42949672956);
+	printf("|%u|\n", -1);
+	printf("|%i|\n", 2147483647);
+	printf("|%d|\n", 2147483648);
+	printf("|%i|\n", -2147483649);
+	printf("|%%|\n");
+	printf("|%%d|\n");
+	printf("|%0d|\n", 42);
+	printf("|%10d|\n", 42);
+	printf("|%3d|\n", 15352);
+	printf("|%-9d|\n", 42);
+	printf("|%0.d|\n", 42);
+	printf("|%*d|\n", 3, 32);
+	printf("|%04d|\n", -42);
+	printf("|%.4d|\n", -42);
+	printf("|%0-3d|\n", 42);
+	printf("|%0.6d|\n", 3);
+	printf("|%-.6d|\n", 3);
 	return (0);
 }
